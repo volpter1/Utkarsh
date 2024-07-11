e293f590ad290f9e1b062903600f37e5f9dc83a7
volpter1 <ut8480076@gmail.com>
2024-07-11 14:11:40 +0530

    Day1

---
+++ Day1/loop.java
@@ -0,0 +1,31 @@
+package Day1;
+
+    public class loop {
+        public static void main(String[]rgs){
+
+        
+   /*    int sum=0;
+
+        for(int i=1;i<26;i++){
+        sum=sum+i;
+        System.out.print(sum +",");
+        
+        }*/
+        int k=5 ;
+        while (k<15){
+            if(k%2==0 && k%7==0){
+                System.out.println(k);
+                 }
+                 k++;
+    }
+
+
+
+    }
+
+}
+    
+    
+
+
+
---
+++ Day1/operator.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class operator {
+    public static void main(String[] args) {
+        int a=1 , b=3 ;
+        a=a+b ;
+        System.out.println(a);
+        System.out.println(b);
+
+    
+    }
+}
